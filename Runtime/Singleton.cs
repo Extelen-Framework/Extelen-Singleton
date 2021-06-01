4d43b031aff58e081c7d44186c49bdfe9b01ef4f
Rodrigo Saguer Pacheco <49574554+Extelen@users.noreply.github.com>
2021-05-31 20:25:03 -0400

    Implemented Singleton.

---
+++ Runtime/Singleton.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Singleton
+{
+
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        //Set Params
+
+        //Static
+        private static T m_instance = null;
+        public T Instance { get => m_instance; }
+
+        //Non Static
+        [Header("Singleton")]
+        [SerializeField] private bool m_dontDestroyOnLoad = false;
+
+        //Methods
+
+        //MonoBehaviour
+        protected virtual void Awake()
+        {
+            if (m_instance == null)
+            {
+                m_instance = this as T;
+                if (m_dontDestroyOnLoad) DontDestroyOnLoad(gameObject);
+            }
+
+            else
+            {
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
